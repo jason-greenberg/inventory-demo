ec60069cde2f8a67de1b456d941cda8b5e001197
jason-greenberg <jasongreenberg07@gmail.com>
2023-01-29 18:03:21 -0800

    Create ProductView component

---
+++ src/components/ProductView/ProductView.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import ProductListItem from '../ProductListItem';
+import ProductDetails from '../ProductDetails';
+import './ProductView.css';
+
+function ProductView({ products }) {
+
+  return (
+    
+  )
+}
