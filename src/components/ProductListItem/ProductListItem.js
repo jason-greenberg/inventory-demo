6fb51a3f696cc0fee59c6ed48c51567fbdadde20
jason-greenberg <jasongreenberg07@gmail.com>
2023-01-29 17:57:46 -0800

    Fill out product list item jsx

--- src/components/ProductListItem/ProductListItem.js
+++ src/components/ProductListItem/ProductListItem.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function ProductListItem({ product, isSelected, onClick }) {
   return (
-
+    <div className={`product-list-item ${isSelected ? 'selected' : ''}`}> {/* Toggle component visibility w/ props */}
+      <img className="product-list-item-photo"
+        src={product.photo.filename} 
+        alt={`${product.name}`} 
+      />
+      <button onClick={onClick}>{product.name}</button>
+    </div>
   )
 }
 
